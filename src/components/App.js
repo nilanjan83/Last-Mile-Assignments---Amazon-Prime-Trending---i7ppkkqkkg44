97c12555bdc2d53e0d5dabca54a6681e2d7883de
nilanjan83 <nilanjandey65@gmail.com>
2021-09-09 20:17:08 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,24 @@ import slides from "../data";
 import '../styles/App.css';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  carouselRoot:{
+    minHeight: 200,
+    "& ul":{
+      marginTop:"50px"
+    }
+  },
+  carouselPaper:{
+    width: "50%",
+    margin:"auto"
+  }
+}));
 
 const App = () => {
+  const classes = useStyles();
   const handleReClick = (index) =>{
     
   }
@@ -16,15 +32,15 @@ const App = () => {
   }
   return (
     <>
-    <Carousel showArrows={true}>
-    <div>{slides.map((item,index) =>{
-      return(
-        <>
-        <h1 data-testid="title">{item.title}</h1>
-        <p data-testid="text">{item.text}</p>
-        </>
-      )
-    })}</div>
+    <Carousel showArrows={true} className={classes.carouselRoot}>
+      {slides.map((item,index) =>{
+        return(
+          <Paper className={classes.carouselPaper}>
+          <h1 data-testid="title">{item.title}</h1>
+          <p data-testid="text">{item.text}</p>
+          </Paper>
+        )
+      })}
     </Carousel>
     <button data-testid="button-restart" onClick={()=>handleReClick(index)}>Restart</button>
     <button data-testid="button-prev" onClick={()=>handlePrevClick(index)}>Prev</button>
